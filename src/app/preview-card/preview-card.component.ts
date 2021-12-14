098c113dc6de86c3d703ce36914beb329d4127f5
royeradames <royeraadames@gmail.com>
2021-12-14 10:16:43 -0500

    📦let preview-card handle server call

--- src/app/preview-card/preview-card.component.ts
+++ src/app/preview-card/preview-card.component.ts
@@ -1,12 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export type Workout = {
+  id: string;
+  name: string;
+  image: string;
+  len: string;
+  level: string;
+  product: string;
+  updatedAt: string;
+};
 @Component({
   selector: 'app-preview-card',
   templateUrl: './preview-card.component.html',
   styleUrls: ['./preview-card.component.scss'],
 })
 export class PreviewCardComponent implements OnInit {
-  constructor() {}
+  /* get workouts from server */
+  workoutList: Workout[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getWorkoutList();
+    console.log(this.workoutList);
+  }
+  configUrl =
+    'https://gist.githubusercontent.com/jasonbyrne/881459829d342a2ddd495165fb815c2d/raw/e0fb08e2fa2a8288a124b1a187b86ecba35d2cb9/echelon-videos-example.json';
 
-  ngOnInit(): void {}
+  getWorkoutList() {
+    this.http.get<any>(this.configUrl).subscribe((data) => {
+      this.workoutList = data.items;
+      console.log(this.workoutList);
+    });
+  }
 }
