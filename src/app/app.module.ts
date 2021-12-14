7f216a01c646bbf39c66c5d50bb4ea2961b94280
royeradames <royeraadames@gmail.com>
2021-12-14 10:11:41 -0500

    📦template for workout elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PreviewCardComponent } from './preview-card/preview-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PreviewCardComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/preview-card/preview-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-card',
+  templateUrl: './preview-card.component.html',
+  styleUrls: ['./preview-card.component.scss'],
+})
+export class PreviewCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
